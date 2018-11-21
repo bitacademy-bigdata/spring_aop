f92aee212911e4adf97e1d5905007a126beb605a
Bit <Bit@DESKTOP-ABVB877>
2018-11-21 15:53:00 +0900

    test

--- src/main/java/com/javalec/common/AroundAdvice.java
+++ src/main/java/com/javalec/common/AroundAdvice.java
@@ -6,9 +6,9 @@ public class AroundAdvice {
 
 	
 	public Object aroungLog(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("[BEFORE] 비즈니스 로직 처리 전 수행!!");
+		System.out.println("[BEFORE] 비즈니스 로직 처리 전 수행!!!");
 		Object returnObj = pjp.proceed() ;
-		System.out.println("[After] 비즈니스 로직 처리 후 수행!!");
+		System.out.println("[After] 비즈니스 로직 처리 후 수행!!!");
 		
 		
 		return returnObj ;
